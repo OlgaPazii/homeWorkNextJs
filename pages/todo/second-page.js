1cddd5665247794760a4c8b284184d52069b4d43
OlgaPazii <64186979+OlgaPazii@users.noreply.github.com>
2020-07-29 14:08:09 +0300

    forth commit

--- pages/todo/second-page.js
+++ pages/todo/second-page.js
@@ -1,3 +1,22 @@
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+  // const res = await fetch(`https://.../data`)
+  // const data = await res.json()
+
+  // Pass data to the page via props
+  return { 
+    props: { 
+      serverSide: (new Date()).toISOString(), 
+    } 
+  }
+}
+
 export default function SecondPost() {
-  return <h1>Second Page!!! Hi here!</h1>
+  return (
+    <div>
+      <h1>Second Page!!! Hi there!!!!!!!!!</h1>
+      {props.serverSide}
+    </div>
+  );
 }
