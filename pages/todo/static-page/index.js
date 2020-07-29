2327ee09529581bb3f9c089877e689bb1a7a851c
OlgaPazii <64186979+OlgaPazii@users.noreply.github.com>
2020-07-29 11:40:17 +0300

    sec commt

--- pages/todo/static-page/index.js
+++ pages/todo/static-page/index.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 
 export async function getStaticProps() {
+  
   // Call an external API endpoint to get posts
   //const res = await fetch('https://.../posts')
   //const posts = await res.json()
